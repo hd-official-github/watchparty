dec04546231c2c8db32c84d2574cca5526dc943f
HiDraqula <aniketaz190@gmail.com>
2021-10-06 01:10:15 +0530

    chat added

--- client/src/chat/chat.jsx
+++ client/src/chat/chat.jsx
@@ -1,8 +1,9 @@
-import { Button, Card } from "@mui/material";
+import { Button, Card, TextField } from "@mui/material";
 import "./chat.scss";
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import { Redirect, useLocation } from "react-router-dom";
+import SendIcon from "@mui/icons-material/Send";
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -10,9 +11,10 @@ let socket,
   allMessages = [];
 const ENDPOINT = "http://localhost:8000";
 export default function Chat({ history }) {
-  const [cHat, setcHat] = useState([]);
+  const [user, setUser] = useState({});
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  // const input = useRef(null);
 
   const [messages, setMessages] = useState([]);
 
@@ -27,29 +29,48 @@ export default function Chat({ history }) {
       setName(name);
       setRoom(room);
 
-
       socket = io.connect(ENDPOINT);
       //join room  on page load
-      socket.emit('joinroom', { name, room });
+      socket.emit("joinroom", { name, room });
 
       //when current user reiceves message
+      socket.on("welcome", (payload) => {
+        console.log({ payload });
+        setUser(payload.client);
+        onMessage(payload);
+      });
       socket.on("message", onMessage);
-
-
-
-
-
     }
     return () => socket.disconnect();
   }, []);
+
   const onMessage = (data) => {
     console.log("D ", data, messages);
-    addMessage({ id: Math.random().toString(), message: data });
+    addMessage({
+      // id: Math.random().toString(),
+      id: data.id,
+      message: data.message,
+      user: data.client,
+    });
   };
   const addMessage = (data) => {
-    console.log({ messages });
+    console.log({ messages, data });
     allMessages.push(data);
-    setMessages(allMessages.slice(-100));
+    // setMessages(allMessages.slice(-100));
+    setMessages([...allMessages]);
+  };
+  const hSendMessage = (e) => {
+    // console.log({ ...e });
+    e.preventDefault();
+    // const message = input.current.value;
+    // input.current.value = "";
+
+    const message = e.target[0].value;
+    // console.log({ message });
+    if (message) {
+      e.target[0].value = "";
+      socket.emit("chatMessage", message);
+    }
   };
   return (
     <>
@@ -64,12 +85,31 @@ export default function Chat({ history }) {
             Logout
           </Button>
         </nav>
-        <div className="messages flex col">
-          {messages.map((m) => (
-            <span key={m.id} className="left">
-              {m.message}
-            </span>
-          ))}
+        <div className="chatbox">
+          <div className="messages flex col jc-end mb-1">
+            {messages.map((m) => (
+              <span
+                key={m.id}
+                className={m.user?.id == user?.id ? "right" : "left"}
+              >
+                {m.message}
+              </span>
+            ))}
+          </div>
+          <form className="input flex" onSubmit={hSendMessage}>
+            <TextField
+              // id="standard-basic"
+              // inputRef={input}
+              fullWidth
+              label="Type Message"
+              variant="standard"
+              name="message"
+            />
+            <Button variant="contained" type="submit">
+              Send&nbsp;
+              <SendIcon fontSize="small" />
+            </Button>
+          </form>
         </div>
       </main>
     </>
--- server/src/micorservices/socket.js
+++ server/src/micorservices/socket.js
@@ -8,26 +8,45 @@ const soc = new Server(server, { cors: { origin: "*" } });
 soc.on("connection", (socket) => {
   // console.log('CCE  ', socket);
 
-  socket.on('joinroom', ({ name, room }) => {
-    socket.emit('message', `Welcome to chat ${name}`)  //welcome current user
+  socket.on("joinroom", ({ name, room }) => {
+    const user = addUser(socket.id, name, room); //add user to temp memory
+    socket.join(user.room); //add user to room
 
-    const user = addUser(socket.id, name, room)  //add user to temp memory
-    socket.join(user.room) //add user to room
+    const payload = {
+      message: `Welcome to chat ${name}`,
+      client: { id: user.id, name: user.username },
+    };
+    //socket.emit("message", payload); //welcome current user
+    socket.emit("welcome", payload); //welcome current user
 
-    socket.broadcast.to(user.room).emit('message', `${user.username} has joined`)  //broadcast to the room except current user
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { ...payload, message: `${user.username} has joined` }); //broadcast to the room except current user
   });
 
-
-  // socket.on("chatMessage", (msg) => {
-  //   soc.emit("message", `${msg} joined the chat`);
-  // });
+  socket.on("chatMessage", (msg) => {
+    // socket.emit("message", msg);
+    const user = getCurrentUser(socket.id);
+    const payload = {
+      id: Math.random().toString(), // document/message id from database
+      message: msg,
+      client: { id: user.id, name: user.username },
+    };
+    socket.emit("message", payload); // might have to remove this & make both emits as one
+    socket.broadcast.to(user.room).emit("message", payload);
+  });
 
   socket.on("disconnect", () => {
     const user = getCurrentUser(socket.id);
     if (user) {
-      soc.to(user.room).emit("message", `${user.username} left the chat`);
+      const payload = {
+        id: Math.random().toString(),
+        message: `${user.username} left the chat`,
+        client: { id: user.id, name: user.username },
+      };
+      soc.to(user.room).emit("message", payload);
     } else {
-      soc.emit('message', 'Somebody left the chat')
+      soc.emit("message", { message: "Somebody left the chat" });
     }
   });
 });
